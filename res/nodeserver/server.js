436df6d86e264ab9278100102b11900c3988abea
G. G. <suricator@free.fr>
2025-08-09 14:21:30 +0100

    Nodemodule ignored

--- res/nodeserver/server.js
+++ res/nodeserver/server.js
@@ -1,1 +1,9 @@
-console.log("Hello World");
+let express = require('express');
+let app = express();
+logger = require('./src/logger.js').logger;
+
+app.set('port', process.env.PORT || 2258);
+
+var server = app.listen(app.get('port'), function () {
+            logger.info('server.js: Express server listening on port ' + server.address().port);
+        });
